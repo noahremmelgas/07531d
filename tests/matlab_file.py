a23843eb1e5ea85d1db82b80171127b89fdcb3a8
Noah Remmelgas <noahre@chalmers.se>
2025-03-25 19:36:56 +0100

    ufhfjaS

---
+++ tests/matlab_file.py
@@ -0,0 +1,42 @@
+from scipy.io import loadmat
+import numpy as np
+import h5py
+
+subcarrier_num = 57
+bw = 20*10**6
+c = 299792458
+data = loadmat('tests/csi_src_test.mat')
+data = data['csi']
+
+data = np.load('tests/npz_files/loc_30deg_1m_reduced.npz')
+data = data['arr_0']
+
+data_shape = data.shape
+
+ifft_point = int(np.power(2, np.ceil(np.log2(data_shape[1]))))
+print(ifft_point)
+cir_sequence = np.fft.ifft(data, ifft_point, 1)
+
+#cir_sequence = np.squeeze(np.mean(cir_sequence, 3)) #Behövs inte för NPZ filerna
+
+print(cir_sequence.shape)
+
+half_point = int(ifft_point/2)
+
+half_sequence = cir_sequence[:, 0:half_point, :]
+
+magnitudes = np.abs(half_sequence)
+peak_values = np.max(magnitudes, axis=1)
+peak_indices = np.argmax(magnitudes, axis=1)
+
+peak_indices = np.squeeze(peak_indices) + 1
+
+print(peak_indices)
+
+tof_mat = (peak_indices * subcarrier_num) / (ifft_point * bw)
+
+print(tof_mat)
+
+est_distance = np.mean(tof_mat*c)
+
+print(est_distance)
--- tests/new_file.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import h5py
-f = h5py.File('csi_src_test.mat','r')
-data = f.get('csi')
-data = np.array(data) # For converting to a NumPy array
-print(data)
